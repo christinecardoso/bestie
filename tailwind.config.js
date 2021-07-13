41ced5c099290007d50fa54ca9f61c1b5b3395d9
christinecardoso <christinesalafia@gmail.com>
2021-07-13 17:34:12 -0400

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    './src/**/*.njk',
+    './src/**/*.md'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
